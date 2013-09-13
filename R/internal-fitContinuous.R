b9d34f6e5c6d13add93ee37adcbdc6fb2ae1e52e
Rich FitzJohn <rich.fitzjohn@gmail.com>
2013-09-13 15:08:49 +1000

    Internal functions for pulling apart fitContinuous models
    Generic functions for extracting the model name (e.g., BM, OU), the
    underlying data, the fitted parameters, and an object that combines
    all of these that will be useful when making a unit tree.
    
    None of these are exported, and should be seen as liable to change, as
    we work out what we really need.  I don't see that the user should ever
    need to care what is going on here though.
    
    This relies on the current version of geiger, so updated DESCRIPTION to
    reflect this.

---
+++ R/internal-fitContinuous.R
@@ -0,0 +1,46 @@
+## Internal functions for pulling apart fitContinuous objects.  These
+## need to be kept in close alignment with geiger, because they use
+## undocumented (and therefore potentially unstable) features of
+## geiger.
+
+## Some of these could be merged into geiger, but we're going to need
+## similar features for every model type and people won't do this for
+## us.  So instead, we'll write some unit tests that check that things
+## are behaving as expected.  Then we should at least get fairly early
+## warning when things break.  Later on, we can look at rolling some
+## interface into the different packages if people will let us.
+
+## Note that we depend on version 2.0 of geiger, and previous versions
+## *will not work*, here.
+
+## Note even though these aren't actually S3 generics (yet) the
+## function names are written as though they are.
+modeltype.gfit <- function(fit, ...) {
+  ret <- attr(fit$lik, "model")
+  if (is.null(ret) || !is.character(ret) || length(ret) != 1)
+    stop("Failed to extract model type from geiger fit")
+  ret
+}
+
+modeldata.gfit <- function(fit, ...) {
+  e <- environment(fit$lik)
+  list(phy=get("phy", e), data=get("dat", e))
+}
+
+modelpars.gfit <- function(fit, ...) {
+  model <- modeltype.gfit(fit)
+  if (model %in% c("BM", "OU", "EB")) {
+    pars <- as.list(coef(fit))
+    if (!("SE" %in% names(pars)))
+      pars$SE <- 0
+    pars[c(setdiff(names(pars), "SE"), "SE")]
+  } else {
+    stop(sprintf("Cannot extract from models of model %s", model))    
+  }
+}
+
+modelinfo.gfit <- function(fit, ...) {
+  list(data=modeldata(fit),
+       pars=modelpars(fit),
+       type=modeltype(fit))
+}
