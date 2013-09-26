c316497a7ed2ba5a2cbe24e4abcc77fd0b6c7f4c
mwp <mwpennell@gmail.com>
2013-09-25 17:32:06 -0700

    modified the fxn sim.charUnit() such that it now takes either a single unit tree or a list of unit trees. If a list is supplied, it performs a single simulation on each unit tree. If a single tree is supplied, it performs nsim simulations on the single unit tree

--- R/sim.charUnit.R
+++ R/sim.charUnit.R
@@ -10,17 +10,28 @@
 
 ## nsim -- the number of simulations
 
+## Note that if it is given one unit.tree, the fxn simulates nsim datasets on the unit tree
+## If given a list of unit.trees, the fxn simulates one dataset per tree and ignores the nsim command
 
-sim.charUnit <- function(unit.tree, nsim){
-	if (!inherits(unit.tree, "unit.tree"))
-		stop("unit.tree must be of class 'unit.tree'")
-		
-	phy <- unit.tree$phy	
-	
-	dat <- sim.char(phy, par=1, nsim=nsim, model="BM")[,,]
-	
-	ut <- lapply(1:nsim, function(x) as.unit.tree(phy, dat[,x]))
-	
+
+sim.charUnit <- function(unit.tree, nsim=1000){
+	if (inherits(unit.tree, "unit.tree")){ ## simulate on one tree
+
+                phy <- unit.tree$phy
+                dat <- sim.char(phy, par=1, nsim=nsim, model="BM")[,,]
+                ut <- lapply(1:nsim, function(x) as.unit.tree(phy, dat[,x]))
+                
+            } else { ## simulate one simulation per tree in list
+
+                ## first check to make sure first element is a unit.tree
+                if (!is.unit.tree(unit.tree[[1]]))
+                    stop("unit.tree must be either a single unit tree or a list of unit.trees")
+
+                multi.phy <- lapply(unit.tree, function(x) return(x$phy))
+                multi.dat <- lapply(multi.phy, function(x) return(sim.char(x, par=1, model="BM")[,,]))
+                ut <- lapply(1:length(multi.phy), function(x) as.unit.tree(multi.phy[[x]], multi.dat[[x]]))
+            }
+                                    
 	ut
 	
 }
